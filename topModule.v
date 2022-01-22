5cacaa65ad71f6c6d07bf0ea0305a57d573c05d1
Ashik M M <80384624+ashikmm3010@users.noreply.github.com>
2022-01-22 17:16:16 +0530

    Update topModule.v
    top module output verified

--- topModule.v
+++ topModule.v
@@ -19,59 +19,62 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module top(
-    input [12:0] sensorL,
-    input [12:0] sensorC,
-    input [12:0] sensorR,
+module roughModule(
+    input wire [9:0] sensorL,
+    input wire [9:0] sensorC,
+    input wire [9:0] sensorR,
     input clock,
-	output reg AF,AB,BF,BB);	//its assumed that motors rotate using active low levels.
+    output reg AF,AB,BF,BB);    //taken as active low
     
+	ColourSensorController(s,clk,OE,colour,freq);	//sub module wrapping
+	
+	reg black = 12'd1000;
+	reg white = 12'd30;
 	reg [2:0] a;
-	reg black = 1023;
-	reg white = 32;
+	
 
 //orientation of the movement & Node Detection//
 
-	always@(posedge clock)
-	begin
+always@(posedge clock)
+begin
 
-		if(sensorL < black)  a[2] = 1;
-		else  a[2] = 0;
+	if(sensorL > 30)  a[2] = 1;
+	else if(sensorL < 30) a[2] = 0;
 
-		if(sensorC < black) a[1] = 1;
-		else a[1] = 0;
+	if(sensorC > 30) a[1] = 1;
+	else if(sensorL < 30) a[1] = 0;
 
-		if(sensorR < black) a[0] = 1;
-		else a[0] = 0;
+	if(sensorR > 30) a[0] = 1;
+	else if(sensorL < 30) a[0] = 0;
 
-	end
+end
 
-	always@(posedge clock)
-	begin
-		case(a)
+always@(posedge clock)
+begin
+case(a)
 
-			3'b010:begin AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1;end//Move straight
-			3'b100:begin AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1; end//Move right
-			3'b001:begin AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1; end//Move left
-
-
-			3'b110:begin //Node Detected (on left)
-
-						//send message to Xbee
-
-						AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1; end
-
-			3'b011:begin //Node Detected (on right) then turn right
-
-						//send message to Xbee
-
-						AF=1'b0;AB=1'b1;BF=1'b1;BB=1'b1; end
+	3'b010:begin AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1;end//Move straight
+	3'b100:begin AF=1'b1;AB=1'b1;BF=1'b0;BB=1'b1; end//Move right
+	3'b001:begin AF=1'b0;AB=1'b1;BF=1'b1;BB=1'b1; end//Move left
 
 
-			default:begin AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1; end//stop
+	3'b110:begin //Node Detected (on left)
 
-		endcase
+				//send message to Xbee
 
-	end
+				AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1; end
 
+	3'b011:begin //Node Detected (on right)  then turn right
+
+				//send message to Xbee
+
+				AF=1'b0;AB=1'b1;BF=1'b1;BB=1'b1; end
+
+
+	//default:begin AF=1'b0;AB=1'b1;BF=1'b0;BB=1'b1; end//stop
+
+endcase
+
+end
+////////////////////////////////////////////////////////////////////////);
 endmodule
